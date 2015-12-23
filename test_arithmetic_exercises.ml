d46ebd59c30f287e31160eadb200395fb20d178b
ericlippert <eric@lippert.com>
2015-12-22 21:19:11 -0800

    Add tests for phi and factorization methods
    Add tests for phi and factorization methods

--- test_arithmetic_exercises.ml
+++ test_arithmetic_exercises.ml
@@ -7,7 +7,21 @@ open Test_exercises;;
 
 let test_is_prime () =
     test_many "is_prime" is_prime (range 0 6) [false; false; true; true; false; true; false ] string_of_bool;;
+    
+let test_phi () =
+    test_many "phi" phi (range 0 10) [0; 0; 0; 1; 1; 3; 1; 5; 3; 5; 3] string_of_int;;
+    
+let test_factors () =
+    test_many "factors" factors (range 1 12) [[1]; [2]; [3]; [2; 2]; [5]; [2; 3]; [7]; [2; 2; 2]; [3; 3]; [2; 5]; [11]; [2; 2; 3]] string_of_int_list;;
+
+let test_factorization () =
+    test_many "factorization" factorization (range 1 12) 
+        [[(1, 1)]; [(1, 2)]; [(1, 3)]; [(2, 2)]; [(1, 5)]; [(1, 2); (1,3)]; [(1, 7)]; [(3, 2)]; [(2, 3)]; [(1, 2); (1, 5)]; [(1, 11)]; [(2, 2); (1, 3)]] 
+        string_of_int_int_tuple_list;;
 
 let test_arithmetic () =
     test_is_prime ();
+    test_phi ();
+    test_factors ();
+    test_factorization ();
     ;;
