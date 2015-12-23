8d2c4b2bd2213e811b584efcb25d4700d84924cd
ericlippert <eric@lippert.com>
2015-12-22 21:59:01 -0800

    build-and-run batch file bails out on error

--- doit.bat
+++ doit.bat
@@ -1,7 +1,17 @@
 ocamlc -c printing.ml
+@if errorlevel 1 goto failure
 ocamlc -c arithmetic_exercises.ml
+@if errorlevel 1 goto failure
 ocamlc -c list_exercises.ml
+@if errorlevel 1 goto failure
 ocamlc -c test_exercises.ml
+@if errorlevel 1 goto failure
 ocamlc -c test_list_exercises.ml
+@if errorlevel 1 goto failure
 ocamlc -c test_arithmetic_exercises.ml
+@if errorlevel 1 goto failure
 ocaml printing.cmo list_exercises.cmo arithmetic_exercises.cmo test_exercises.cmo test_list_exercises.cmo test_arithmetic_exercises.cmo tests.ml
+@if errorlevel 1 goto failure
+exit
+:failure
+@echo FAILURE
